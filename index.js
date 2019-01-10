dee37d44dd5e2c3bf8481d29cfdfddd4c752246b
pavelhristov <pavel.hristov_@abv.bg>
2019-01-11 01:05:42 +0200

    initial

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const SocketIO = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+const io = SocketIO(server);
+
+const PORT = 3001;
+
+app.use(express.static('./public'));
+
+app.get('/', function (req, res) {
+    res.send('index.html');
+});
+
+io.on('connection', function (socket) {
+    let username = socket.handshake.query.name;
+    socket.broadcast.emit('user control', { message: username + ' connected' });
+
+    socket.on('chat message', function (data) {
+        socket.broadcast.emit('chat message', { username, message: data.message });
+    });
+
+    socket.on('send file', function (data) {
+        socket.broadcast.emit('send file', { username, file: data.file, type: data.type });
+    });
+
+    socket.on('disconnect', function () {
+        socket.broadcast.emit('user control', { message: username + ' disconnected!' });
+    });
+});
+
+server.listen(PORT, function () {
+    console.log('listening on *:' + PORT);
+})
+
---
+++ public/scripts/sockets.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const container = document.getElementById('container');
+const MESSAGE_TYPE = {
+    SYSTEM: 'system',
+    IMAGE: 'image'
+}
+
+container.querySelector('button').addEventListener('click', function (ev) {
+    let username = this.previousElementSibling.value || '';
+    username = username.trim();
+    if (!username || username.length < 3) {
+        alert('you need an user name thats lengthier than 3!');
+        return false;
+    }
+
+    container.innerHTML = `
+    <div>
+        <div class="messages"></div>
+        <input class="input-message"/>
+        <button class="btn-message">Send Message</button>
+
+        <br>
+        <input type="file" class="input-file" accept="image/*"/>
+    </div>`;
+    setSockets(username);
+});
+
+function setSockets(username) {
+    const socket = io('', { query: `name=${username}` });
+
+    const input = container.querySelector('.input-message');
+    container.querySelector('.btn-message').addEventListener('click', function (ev) {
+        let message = input.value;
+        if (!message || message.length < 1) {
+            return false;
+        }
+
+        input.value = '';
+        socket.emit('chat message', { message });
+        showMessage(username, message);
+    });
+
+    socket.on('chat message', function (data) {
+        showMessage(data.username, data.message);
+    });
+
+    socket.on('user control', function (data) {
+        showMessage(null, data.message, MESSAGE_TYPE.SYSTEM);
+    });
+
+    let messagesContainer = container.querySelector('.messages');
+    function showMessage(username, message, type) {
+        let content = '';
+        switch (type) {
+            case MESSAGE_TYPE.SYSTEM:
+                content = `<i>${message}</i>`;
+                break;
+            case MESSAGE_TYPE.IMAGE:
+                content = `${username}: <img src="data:image/jpeg;base64,${message}" height="150" />`;
+                break;
+
+            default:
+                content = `${username}: ${message}`;
+                break;
+        }
+
+        let div = document.createElement('div');
+        div.innerHTML = content;
+        messagesContainer.appendChild(div);
+    }
+
+
+    const fileInput = container.querySelector('.input-file');
+    fileInput.addEventListener('change', function (ev) {
+        let file = fileInput.files[0];
+        if (!file) {
+            return false;
+        }
+
+        // TODO: stream it, chunk it, slice it, chop it!
+        let reader = new FileReader();
+        reader.readAsBinaryString(file);
+
+        reader.onload = function () {
+            let result = btoa(reader.result)
+            socket.emit('send file', { file: result, type: 'image' });
+            showMessage(username, result, MESSAGE_TYPE.IMAGE);
+        };
+        reader.onerror = function () {
+            console.error('Error while reading the file!');
+        };
+    });
+
+    socket.on('send file', function (data) {
+        showMessage(data.username, data.file, data.type);
+    });
+}
